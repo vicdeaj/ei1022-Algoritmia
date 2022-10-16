a11ad9d9d070057391e78fbc02ff1045b9b4ffd7
vik0t0r <al404303@uji.es>
2022-10-16 20:23:36 +0200

    fivee points

--- entregables/entregable1/entregable1.py
+++ entregables/entregable1/entregable1.py
@@ -6,6 +6,8 @@ from typing import TextIO, Optional
 from algoritmia.datastructures.graphs import UndirectedGraph
 from algoritmia.datastructures.mergefindsets import MergeFindSet
 
+from algoritmia.algorithms.shortest_path import shortest_path_unweighted_graph
+
 Vertex = tuple[int, int]
 Edge = tuple[Vertex, Vertex]
 
@@ -33,15 +35,108 @@ def create_labyrinth(rows: int, cols: int, n: int, s: int) -> UndirectedGraph[Ve
 
 
 def read_data(f: TextIO) -> tuple[UndirectedGraph[Vertex], int, int]:
-    raise NotImplementedError()
+    rows = int(f.readline())
+    cols = int(f.readline())
+    removeN = int(f.readline())
+    seed = int(f.readline())
+
+    graph = create_labyrinth(rows, cols, removeN, seed)
+
+    return graph, rows, cols
+
+
+def precalculardist(lab: UndirectedGraph[Vertex], inicio: Vertex, dictresults: dict[Vertex, int]):
+    for v in lab.V:
+        distancia = len(shortest_path_unweighted_graph(lab, inicio, v))
+        dictresults[v] = distancia
+
+
 
 
 def process(lab: UndirectedGraph[Vertex], rows: int, cols: int) -> tuple[Optional[Edge], int, int]:
-    raise NotImplementedError()
+    v_source = (0,0)
+    v_dest = (rows - 1, cols - 1)
+    length_before = len(shortest_path_unweighted_graph(lab, v_source, v_dest))
+    length_after = length_before
+
+
+    dictInit2Cell: dict[Vertex, int] = {}  # diccionario que guarda las distancias desde el inicio a cada posible coordenada del grafo
+    dictCell2End: dict[Vertex, int] = {}  # diccionario que guarda las distancias desde el final a cada posible coordenada del grafo
+
+    wallToDelete = ((999999,999999),(999999,999999))
+
+
+    # precalcular de 0 a todas las celdas
+
+    precalculardist(lab, v_source, dictInit2Cell)
+
+    # precalcular de todas las celdas a la  salida
+
+    precalculardist(lab, v_dest, dictCell2End)
+
+    # recorremos TODAS las aristas y si no estan en el laberinto y son mas corta (el camino por ahí es mas corto) que la actual guardamos la pared
+    for r, c in lab.V:
+
+        if r == 0 and c == 0:
+            continue
+        elif r == 0:
+            a: Edge = ((r, c), (r, c - 1))
+            if a[0] > a[1]:
+                a = (a[1], a[0])
+
+            if a not in lab.E:
+                coste = min(dictInit2Cell[a[0]] + dictCell2End[a[1]], dictCell2End[a[0]] + dictInit2Cell[a[1]])
+                if coste < length_after or (coste == length_after and a < wallToDelete):
+                    length_after = coste
+                    wallToDelete = a
+
+        elif c == 0:
+            a: Edge = ((r, c), (r - 1, c))
+            if a[0] > a[1]:
+                a = (a[1], a[0])
+
+
+            if a not in lab.E:
+                coste = min(dictInit2Cell[a[0]] + dictCell2End[a[1]], dictCell2End[a[0]] + dictInit2Cell[a[1]])
+                if coste < length_after or (coste == length_after and a < wallToDelete):
+                    length_after = coste
+                    wallToDelete = a
+        else:
+            a1: Edge = ((r, c), (r, c - 1))
+            if a1[0] > a1[1]:
+                a1 = (a1[1], a1[0])
+
+            a2: Edge = ((r, c), (r - 1, c))
+            if a2[0] > a2[1]:
+                a2 = (a2[1], a2[0])
+
+            if a1 not in lab.E:
+                coste = min(dictInit2Cell[a1[0]] + dictCell2End[a1[1]], dictCell2End[a1[0]] + dictInit2Cell[a1[1]])
+                if coste < length_after or (coste == length_after and a1 < wallToDelete):
+                    length_after = coste
+                    wallToDelete = a1
+
+            if a2 not in lab.E:
+                coste = min(dictInit2Cell[a2[0]] + dictCell2End[a2[1]], dictCell2End[a2[0]] + dictInit2Cell[a2[1]])
+                if coste < length_after or (coste == length_after and a2 < wallToDelete):
+                    length_after = coste
+                    wallToDelete = a2
+
+    if wallToDelete == ((999999,999999),(999999,999999)):
+        wallToDelete = None
+
+    return wallToDelete, length_before - 1, length_after - 1
 
 
 def show_results(edge_to_add: Optional[Edge], length_before: int, length_after: int):
-    raise NotImplementedError()
+
+    if edge_to_add is None:
+        print(NO_VALID_WALL)
+    else:
+        print(edge_to_add[0][0], edge_to_add[0][1], edge_to_add[1][0], edge_to_add[1][1])
+
+    print(length_before)
+    print(length_after)
 
 
 if __name__ == '__main__':
