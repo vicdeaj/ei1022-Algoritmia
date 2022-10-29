f38417716a05270b041fe0d485a526f137a2b16f
vik0t0r <victordpa7@gmail.com>
2022-10-29 20:10:01 +0200

    added huecos list

--- entregables/entregable2/entregable2.py
+++ entregables/entregable2/entregable2.py
@@ -5,6 +5,7 @@ from typing import TextIO
 # Folleto en inglés es Leaflet
 Leaflet = tuple[int, int, int]          # (num_folleto, anchura, altura)
 LeafletPos = tuple[int, int, int, int]  # (num_folleto, num_hoja_de_imprenta, pos_x ,pos_y)
+Hueco = list[int, int ,int, int, int]   #x0 x1 y0 y1 nHoja
 
 
 # Lee la instancia por la entrada estándar (ver apartado 1.1)
@@ -35,8 +36,10 @@ def process(paper_size: int, leaflet_list: list[Leaflet]) -> list[LeafletPos]:
     sorted_indices = sorted(indices, key=lambda i: -lista_tam[i])
 
     resultado: list[LeafletPos] = []
-    huecos: list[list[int, int, int, int]] = [[0, paper_size, 0, paper_size]]
-    hoja = 1
+
+
+    hojas_total = 1
+    huecos: list[Hueco] = [[0, paper_size, 0, paper_size, hojas_total]]
     for i in sorted_indices:
         folleto: Leaflet = leaflet_list[i]
         #print(folleto)
@@ -45,15 +48,16 @@ def process(paper_size: int, leaflet_list: list[Leaflet]) -> list[LeafletPos]:
         for hueco in huecos:
             anchura = hueco[1] - hueco[0]
             altura = hueco[3] - hueco[2]
+            Hlocal = hueco[4]
             if anchura >= folleto[1] and altura >= folleto[2]:
                 encajado = True
-                resultado.append((folleto[0], hoja, hueco[0], hueco[2]))
+                resultado.append((folleto[0], Hlocal, hueco[0], hueco[2]))
                 x = hueco[0] + folleto[1]
                 y = hueco[2] + folleto[2]
                 if  x != hueco[1] and y != hueco[3]:
                     huecos.remove(hueco)
-                    huecos.append([x, hueco[1], hueco[2], y])
-                    huecos.append([hueco[0], hueco[1], y, hueco[3]])
+                    huecos.append([x, hueco[1], hueco[2], y, Hlocal])
+                    huecos.append([hueco[0], hueco[1], y, hueco[3], Hlocal])
                     break
                 if x == hueco[1]:
                     hueco[2] = y
@@ -61,14 +65,14 @@ def process(paper_size: int, leaflet_list: list[Leaflet]) -> list[LeafletPos]:
                 hueco[0] = x
                 break
         if not encajado:
-            hoja += 1
+            hojas_total += 1
             hueco = [0, paper_size, 0, paper_size]
             huecos: list[list[int, int, int, int]] = []
-            resultado.append((folleto[0], hoja, hueco[0], hueco[2]))
+            resultado.append((folleto[0], hojas_total, hueco[0], hueco[2]))
             x = hueco[0] + folleto[1]
             y = hueco[2] + folleto[2]
-            huecos.append([x, hueco[1], hueco[2], y])
-            huecos.append([hueco[0], hueco[1], y, hueco[3]])
+            huecos.append([x, hueco[1], hueco[2], y, hojas_total])
+            huecos.append([hueco[0], hueco[1], y, hueco[3], hojas_total])
     return resultado
 
 # Muestra por la salida estandar las posiciones de los folletos (ver apartado 1.2)
