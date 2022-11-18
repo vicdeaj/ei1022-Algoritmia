7cfd4c99451fa40f61baf84966f2d9931393e260
mehdi <mehdigassabarsa@gmail.com>
2022-11-18 17:11:20 +0100

    Inicio entregable 3

--- entregables/entregable3/entregable3.py
+++ entregables/entregable3/entregable3.py
@@ -23,15 +23,49 @@ Solution = tuple[Decision, ...]  # Utilizad la función auxiliar 'directions2str
 # ---------------------------------------------------------------------------------------------------------------------
 
 def read_data(f: TextIO) -> Board:
-    raise NotImplementedError()
+    board: Board = Board(f.readlines())
+    return board
+
 
 
 def process(board: Board) -> Optional[Solution]:
-    raise NotImplementedError()
+    @dataclass
+    class Extra:
+        brick: Brick = Brick(board.start_pos(), board.start_pos())
+
+    class KnapsackDS(ScoredDecisionSequence):
+        def is_solution(self) -> bool:  # dice si estamos en una soludcion
+            return self.extra. == board.target_pos()
+
+        def successors(self) -> Iterator[KnapsackDS]:  # añade las dos posibles decisiones
+            n = len(self)
+            if n < len(valorObjetos):
+                if self.extra.current_weight + pesoObjetos[n] <= capacidad:
+                    current_weight2 = self.extra.current_weight + pesoObjetos[n]
+                    current_value2 = self.extra.current_value + valorObjetos[n]
+                    yield self.add_decision(1, Extra(current_weight2, current_value2))
+                yield self.add_decision(0,
+                                        self.extra)  # no metemos objeto en mochila, lo metemos despues, asi llega antes a los 1 y por lo tanto añade mas elementos
+
+        def solution(self):
+            return self.extra.current_value, self.extra.current_weight, self.decisions()
+
+        def state(self):  # cuando dos nodos tienen el mismo futuro (repesentan el mismo problema)
+            return len(self), self.extra.current_weight
+
+        def score(self):
+            return self.extra.current_value
+
+    initial_ds = KnapsackDS(Extra(0, 0))
+    best_sol = list(bt_max_solve(initial_ds))[-1]
+    return best_sol
 
 
 def show_results(solution: Optional[Solution]):
-    raise NotImplementedError()
+    if solution == None:
+        print("INSTANCE WITHOUT SOLUTION")
+    else:
+        directions2string(solution)
 
 
 # Programa principal --------------------------------------------------------------------------------------------------
